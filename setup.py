176fa5bdf5d8c8c5a31be4d0e88c61c8b854d59a
Artur Kuramshin <artur.kurams@gmail.com>
2024-11-16 15:40:22 -0500

    Use src-structure for package

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(name='gym_carla',
       version='0.0.1',
       install_requires=['gym', 'pygame'],
-      package_dir={"": "gym_carla"},
-      packages=find_packages("gym_carla"),
+      package_dir={"": "src"},
+      packages=find_packages("src"),
 )
