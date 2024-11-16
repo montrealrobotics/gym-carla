c343fea0bbf1ad2d8ffa8934710015424929f65e
Artur Kuramshin <artur.kurams@gmail.com>
2024-11-16 12:03:58 -0500

    Fix setup.py install and lidar point detection

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_carla',
       version='0.0.1',
-      install_requires=['gym', 'pygame'] 
+      install_requires=['gym', 'pygame'],
+      package_dir={"": "gym_carla"},
+      packages=find_packages("gym_carla"),
 )
